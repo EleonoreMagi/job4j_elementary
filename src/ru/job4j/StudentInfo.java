05acc9294a12d29575e0189f28493c7f599e4e27
Ellie <nadezda.ryzhkova@gmail.com>
2020-12-28 21:48:08 +0300

    3.3. Разбор первой программы. [#439577]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nadezhda Ryzhkova");
+        System.out.println("28.12.2020");
+
+    }
+}
