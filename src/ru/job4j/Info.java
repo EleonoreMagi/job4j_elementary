c54f77a0cc5cc1dc9f0584864bdfaaa53839a237
Ellie <nadezda.ryzhkova@gmail.com>
2020-12-30 00:02:55 +0300

    3. Арифметические операции. [#439570]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -2,6 +2,14 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("28.12.2020");
+        System.out.println("29.12.2020");
+        byte age = 18;
+        char gender = 'm';
+        float height = 173.9F;
+        int stoneNum = 56798;
+        long bacteriaNum = 13500045323545L;
+        long distanceToJupiter = 4000000000L;
+        boolean workPermit = true;
+        int fileSize = 3050000;
     }
 }
