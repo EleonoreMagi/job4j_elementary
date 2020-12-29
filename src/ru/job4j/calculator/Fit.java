65e438e50cfbc3b3867dc82ce3102212ef2e9ef2
Ellie <nadezda.ryzhkova@gmail.com>
2020-12-30 00:42:52 +0300

    6. Идеальный вес [#439557]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(short height) {
+        double rsl = (height - 110) * 1.15;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        short height = 187;
+        double man = Fit.manWeight(height);
+        double woman = Fit.womanWeight(height);
+        System.out.println("Man with the height of 187 ideally weights " + man + " kilos");
+        System.out.println("Woman with the height of 187 ideally weights " + woman + " kilos");
+    }
+
+}
